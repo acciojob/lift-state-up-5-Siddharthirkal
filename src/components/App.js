33c68319add0fb033542979735e6af902dd017d9
Siddharthirkal <siddharthirkal06>
2026-04-06 20:04:15 +0530

    Added App.js and LoginForm.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import LoginForm from "./LoginForm";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function handleLogin(){
+    setIsLoggedIn(true);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        {isLoggedIn ? (
+          <h2>You are logged in!</h2>
+        ) : (
+          <LoginForm onLogin={handleLogin}/>
+        )}
     </div>
-  )
-}
+  );
+};
 
 export default App
---
+++ src/components/LoginForm.js
@@ -0,0 +1,30 @@
+import React,{useState} from "react";
+function LoginForm({onLogin}){
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+
+    function handleSubmit(e){
+        e.preventDefault();
+        onLogin();
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <div style={{marginBottom:"10px"}}>
+                <label>Username: </label>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+
+            <div style={{marginBottom:"10px"}}>
+                <label>password: </label>
+                <input type="password" value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default LoginForm;
